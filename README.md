e3b457fb7f4fcaff990551cd73b3f3db8776d9a0
Jeff Carter <jcarter00@gmail.com>
2015-12-16 11:10:14 -0500

    updated README and fixed checkWinner and newGame functions

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# project-1-tic-tac-toe
+# Jeff's Project 1: Tic Tac Toe
+
+## Links
+
+* [Github](https://github.com/jrc00/project-1-tic-tac-toe)
+* [Bitballon](http://jc-tictactoe.bitballoon.com/)
+
+## Issues
+
+## What's Next
+
--- js/script.js
+++ js/script.js
@@ -28,58 +28,71 @@ var winningCombo = [
 var playerTurn = 'X';
 var counter = 0;
 
+function doSquareClick() {
+    var square = $(this);
+    // console.log(square);
+    square.prepend(playerTurn);
+    square.off('click'); // freeze square after player's turn
+    game[this.id] = playerTurn; // add each move to game array
+    console.log(game);
+    counter++;
+
+    var winner = checkWinner();
+    console.log('winner: ' + winner);
+
+    // check for tie
+    if (counter === 9) {
+    // sweetalert prompt
+        swal({
+            title: "Cat's Game",
+            text: "Try Again",
+            imageUrl: "images/grumpycat.png"
+        });
+    }
+    playerTurn = playerTurn === 'X' ? 'O' : 'X'; // toggle between players
+}
+
+function checkWinner() {
+    console.log('checkWinner: game = ' + game);
+    var xWins = winningCombo.some(function(wc) {
+        return game[wc[0]] === 'X' && game[wc[1]] === 'X' && game[wc[2]] === 'X';
+    });
+    if (xWins) {
+        return 'X';
+    }
+
+    var oWins = winningCombo.some(function(wc) {
+        return game[wc[0]] === 'O' && game[wc[1]] === 'O' && game[wc[2]] === 'O';
+    });
+    if (oWins) {
+        return 'O';
+    }
+
+    return null;
+}
+
 // GAME PLAY
 $(document).ready(function() {
     console.log('ready');
 
     // Alternate Players and Log Moves to Console
-    $('.square').click(function() {
-        var square = $(this);
-        // console.log(square);
-        square.prepend(playerTurn);
-        square.off('click'); // freeze square after player's turn
-        game[this.id] = playerTurn; // add each move to game array
-        console.log(game);
-        counter++;
-        // check for tie
-        if (counter === 9) {
-        // sweetalert prompt
-            swal({
-                title: "Cat's Game",
-                text: "Try Again",
-                imageUrl: "images/grumpycat.png"
-            });
-        }
-        playerTurn = playerTurn === 'X' ? 'O' : 'X'; // toggle between players
-    });
-
-    // check for winner - DRAFT
-    function checkWinner() {
-        for (k in this.winningCombo) {
-            var pattern = this.winningCombo[k];
-            var p = this.game[pattern[0]] + this.game[pattern[1]] + this.game[pattern[2]];
-            if (p === 'XXX') {
-                return 'X'; // X wins
-            } else if (p === 'OOO') {
-                return 'O'; // O wins
-            };
-        };
-    };
-
-    // log winners and ties
+    $('.square').click(doSquareClick);
 
 
-    // CSS Fade Action on Mouseover
-    $('.square').hover(function() {
-      $(this).fadeOut(200);
-      $(this).fadeIn(200);
-    });
+// log winners and ties
+
+
+// CSS Fade Action on Mouseover
+$('.square').hover(function() {
+    $(this).fadeOut(200);
+    $(this).fadeIn(200);
+});
 });
 
 // START A NEW GAME
 $('#restart').click(newGame);
   function newGame() {
-    $('.square').on('click'); //unfreeze squares
+    $('.square').on('click', doSquareClick  ); //unfreeze squares
     game = [null, null, null, null, null, null, null, null, null]; // resets board
     playerTurn = 'X';
     $('.square').text(''); // clear squares
