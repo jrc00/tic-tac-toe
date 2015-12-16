cf43df14435c9df37de0697e7cd98b3d16380f5d
Jeff Carter <jcarter00@gmail.com>
2015-12-15 20:07:34 -0500

    add background image

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 // var playerX = prompt('Player 1 - enter name');
 // var playerO = prompt('Player 2 - enter your name')
 
-// --- GLOBAL VARIABLES ---
+// GLOBAL VARIABLES
 var game = [null, null, null, null, null, null, null, null, null];
 var winningCombo = [
   [0, 1, 2],
@@ -17,41 +17,53 @@ var winningCombo = [
 var playerTurn = 'X';
 var counter = 0;
 
-// --- GAME PLAY ---
+// GAME PLAY
 $(document).ready(function () {
   console.log('ready');
 
-// --- Alternate Players and Log Moves to Console ---
+// Alternate Players and Log Moves to Console
   $('.square').click(function() {
     var square = $(this);
     console.log(square);
     square.prepend(playerTurn);
-    square.off('click', '**'); // freeze square after player's turn
-    game[this.id] = playerTurn; // add move to game array
+    square.off('click'); // freeze square after player's turn
+    game[this.id] = playerTurn; // add each move to game array
     console.log(game);
-    playerTurn = playerTurn === 'X' ? 'O' : 'X';
+    playerTurn = playerTurn === 'X' ? 'O' : 'X'; // toggle between players
   });
 
-
 // check for winner
-
+function checkWinner() {
+  for (k in this.winningCombo) {
+    var pattern = this.winningCombo[k];
+    var p = this.game[pattern[0]] + this.game[pattern[1]] + this.game[pattern[2]];
+    if (p === 'XXX') {
+      return 'X'; // X wins
+    } else if (p === 'OOO') {
+      return 'O'; // O wins
+    };
+  };
+};
 
 // check for tie
 
 
-// --- CSS Fade Action on Mouseover ---
+// log winners and ties
+
+
+// CSS Fade Action on Mouseover
   $( '.square' ).hover(function() {
     $(this).fadeOut();
     $(this).fadeIn();
   });
 });
 
-// --- START A NEW GAME ---
-$('#restart').on('click', newGame); // event listener
-function newGame() {
-  game = [null, null, null, null, null, null, null, null, null]; // resets board
-  playerTurn = 'X';
-  $('.square').text(''); // clear squares
-  $('.square').on('click', '**');
+// START A NEW GAME
+$('#restart').click(newGame);
+  function newGame() {
+    game = [null, null, null, null, null, null, null, null, null]; // resets board
+    playerTurn = 'X';
+    $('.square').text(''); // clear squares
+    $('.square').on('click'); //unfreeze squares
 }
 
